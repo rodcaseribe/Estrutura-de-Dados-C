85ad63f3d925ad19127ce0bd1e56351875b03af2
rodcaseribeiro <rodolfo2804@hotmail.com>
2020-12-22 17:40:46 -0300

    ainda nao concluido

--- quickMergeBubleSort/ordenacao.cpp
+++ quickMergeBubleSort/ordenacao.cpp
@@ -1,12 +1,74 @@
 #include <stdlib.h> //funções malloc() e free()
-#include<stdio.h>
-#include <conio.h>
+#include<stdio.h> //padrao
 #include <time.h> //calculando tempo
 
+//merge
+void merge(int vetor[], int tamanho) { 
+	    int meio = tamanho / 2;
+	    int i = 0, j = meio, k = 0;
+	    int aux[tamanho];
+	
+	  	while( i < meio && j < tamanho ){
+	    	if( vetor[i] <= vetor[j] )
+	      		aux[k] = vetor[i++];
+	    	else
+	      		aux[k] = vetor[j++];
+	    	k++;
+	  	}
+	 
+	  	if( i == meio )
+	    	while( j < tamanho )
+	      		aux[k++] = vetor[j++];
+	  	else
+	    	while( i < meio )
+	      		aux[k++] = vetor[i++];
+	 
+	  	for( i = 0; i < tamanho; i++ )
+	    	vetor[i] = aux[i];
+}
+ 
+//mergeSort
+int mergeSort(int vetor[], int tamanho){ 
+		int meio = tamanho / 2;
+		if( tamanho > 1 ){
+			mergeSort(vetor, meio);
+			mergeSort(vetor + meio, tamanho - meio);
+			merge(vetor, tamanho);
+		}
+
+}
+
+//QuickSort
+void quickSort(int *a, int left, int right) { 
+	    int i, j, x, y;
+	    i = left;
+	    j = right;
+	    x = a[(left + right) / 2];
+	    while(i <= j) {
+	        while(a[i] < x && i < right) {
+	            i++;
+	        }
+	        while(a[j] > x && j > left) {
+	            j--;
+	        }
+	        if(i <= j) {
+	            y = a[i];
+	            a[i] = a[j];
+	            a[j] = y;
+	            i++;
+	            j--;
+	        }
+	    }
+	    if(j > left) {
+	        quickSort(a, left, j);
+	    }
+	    if(i < right) {
+	        quickSort(a, i, right);
+	    }
+}
+
 //bubbleSort
-void bubbleSort (int vetor[], int n) {
-		clock_t t;
-		t = clock();
+void bubbleSort (int vetor[], int n) { 
 	    int k, j, aux,i ;
 	    for (k = 0; k < n; k++) {
 	        for (j = 0; j < n - k; j++) {
@@ -17,20 +79,9 @@ void bubbleSort (int vetor[], int n) {
 	            }
 	        }
 	    }
-	    t = clock() - t; 
-	    printf("\n-> Metodo BubleSort\n\n"); //Imprimindo Elementos
-		for (i = 0;i < n; i++)
-		{
-			printf("%d\n",vetor[i]);
-		}
-		printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); 
 }
-
 //insertionSort
-void insertionSort(int arr[], int n) 
-{ 
-		clock_t t;
-		t = clock();
+void insertionSort(int arr[], int n){  
 	    int i, key, j,tam; 
 	    for (i = 1; i < n; i++) { 
 	        key = arr[i]; 
@@ -41,110 +92,136 @@ void insertionSort(int arr[], int n)
 	        } 
 	        arr[j + 1] = key; 
 	    } 
-	    t = clock() - t; 
-	    printf("\n-> Metodo InsertSort\n\n"); //Imprimindo Elementos
-		for (tam = 0;tam < n; tam++)
-		{
-			printf("%d\n",arr[tam]);
-		}
-		printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); 
 } 
 
 //selectionSort
 void selectionSort(int num[], int tam) { 
-	  	clock_t t;
-	  	t = clock();
-	  	int i, j, min, aux, tamanho;
-	  	for (i = 0; i < (tam-1); i++) {
-	     	min = i;
-	     	for (j = (i+1); j < tam; j++) {
-	       	if(num[j] < num[min]) 
-	         	min = j;
-	     	}
-	     	if (num[i] != num[min]) {
-	       	aux = num[i];
-	       	num[i] = num[min];
-	       	num[min] = aux;
-	     	}
-	  	}
-	  	t = clock() - t;
-	  	printf("\n-> Metodo Select Sort\n\n"); //Imprimindo Elementos
-		for (tamanho = 0;tamanho < tam; tamanho++)
-		{
-			printf("%d\n",num[tamanho]);
+		int i, j, min, aux, tamanho;
+		for (i = 0; i < (tam-1); i++) {
+		    min = i;
+		    for (j = (i+1); j < tam; j++) {
+		        if(num[j] < num[min]) 
+		        	 min = j;
+		    }
+		    if (num[i] != num[min]) {
+		       aux = num[i];
+		       num[i] = num[min];
+		       num[min] = aux;
+		    }
 		}
-		printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); 
 }
 
 
-int tamanhoVetor(int num_componentes){
+int MENU1(int tamanhoVetor){
 		printf("Digite o tamanho do vetor: ");
-		scanf("%d", &num_componentes);
-		return num_componentes;
+  		scanf("%d", &tamanhoVetor);
+  		return tamanhoVetor;
 }
 
-int tipoVetores(int menu1){
+int MENU2(int menu1){
 		printf ("\nDigite 1 p/ valores ordenados\n");
-		printf ("Digite 2 p/ valores randomicos\n");
-		scanf("%d", &menu1);
-		return menu1;
+  		printf ("Digite 2 p/ valores randomicos\n");
+  		scanf("%d", &menu1);
+  		return menu1;
 }
 
-int tipoOrdenacao(int menu2){
+int MENU3(int menu2){
 		printf ("\nDigite 1 p/ Bubble Sort\n");
 		printf ("Digite 2 p/ Insertion Sort\n");
 		printf ("Digite 3 p/ Selection Sort\n");
+		printf ("Digite 4 p/ Quick Sort\n");
+		printf ("Digite 5 p/ Merge Sort\n");
 		scanf("%d", &menu2);
 		return menu2;
 }
 
-int impressaoVetorInicial(int v[], int tipoVetor, int inputPrimario){
-		switch ( tipoVetor )
-			{
-			case 1 :
-				for (int i = 0; i < inputPrimario; i++){
-				    v[i]=i+1;
-				    printf("posicao [%d] com valor %d\n",i,v[i]); //imprimindo vetor em ordem
-			  	}
-			    break;
-			case 2 :
-				for (int i = 0; i < inputPrimario; i++){
-					v[i] = rand(); 		 //randomizando
-					printf("posicao [%d] com valor %d\n",i,v[i]); //imprimindo vetor randomico
-				}
-			}
+int popularVetor(int retornoMenu2, int retornoMenu1, int Vetor[], int i){
+	switch ( retornoMenu2 )
+	  {
+	    case 1 :
+		    for (i = 0; i < retornoMenu1; i++)
+		  	{
+			    Vetor[i]=i+1;
+			    //printf("posicao [%d] com valor %d\n",i,Vetor[i]); //imprimindo vetor em ordem
+		  	}
+	    	break;
+	    case 2 :
+		    for (i = 0; i < retornoMenu1; i++)
+		  	{
+			    Vetor[i] = rand(); 		 //randomizando
+			    //printf("posicao [%d] com valor %d\n",i,Vetor[i]); //imprimindo vetor randomico
+		  	}
+	  }
 }
 
-
-
-int main(void){
+int ordenamentos(int Vetor[], int retornoMenu1, int retornoMenu3){
+	 switch ( retornoMenu3 )
+	  {
 	  	clock_t t;
-		int menu2,menu1;
-		int *v; //definindo o ponteiro v
-		int i, num_componentes, inputPrimario, tipoVetor, tipoDeOrdenacao;
-		inputPrimario = tamanhoVetor(num_componentes);
-		v = (int *) malloc(inputPrimario * sizeof(int)); //Armazenando os dados em um vetor 
-		tipoVetor = tipoVetores(menu1);
-		impressaoVetorInicial(v,tipoVetor,inputPrimario);
-		tipoDeOrdenacao = tipoOrdenacao(menu2);
-	  	switch ( tipoDeOrdenacao )
-  		{
-		    case 1 :
-		    	bubbleSort(v,inputPrimario ); //chamada bubbleSort
-		    	break;
-		    case 2 :
-		    	insertionSort(v,inputPrimario ); //chamada insertionSort
-		    	break;
-		    case 3 :
-		    	selectionSort(v,inputPrimario ); //chamada selectionSort
-		    	break;
-		    default :
-		    	printf ("Valor invalido!\n");
-		}
+	    case 1 :
+	  		t = clock();
+	    	bubbleSort(Vetor,retornoMenu1 ); //chamada bubbleSort
+	    	t = clock() - t;
+	    	printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); 
+	    	break;
+	    case 2 :
+	  		t = clock();
+	    	insertionSort(Vetor,retornoMenu1 ); //chamada insertionSort
+	    	t = clock() - t;
+	    	printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); 
+	    	break;
+	    case 3 :
+	  		t = clock();
+	    	selectionSort(Vetor,retornoMenu1 ); //chamada selectionSort
+	    	t = clock() - t;
+	    	printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000))); 
+	    	break;
+	    case 4 :
+	  		t = clock();
+	    	quickSort(Vetor,0,retornoMenu1-1); //chamada quickSort
+	    	t = clock() - t;
+	    	printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000)));   //mergeSort(vetor, TAM_VETOR);
+	    	break;
+	    case 5 :
+	    	t = clock();
+	    	mergeSort(Vetor, retornoMenu1);; //chamada mergeSort
+	    	t = clock() - t;
+	    	printf("Tempo de execucao em ms: %lf", ((double)t)/((CLOCKS_PER_SEC/1000)));   
+	    	break;
+	    default :
+	    	printf ("Valor invalido!\n");
+	  }
+}
 
-
-		printf("\n Rodolfo Casemiro Ribeiro - LUZ 2020 \n");
-		free(v); //liberando o espaço de memória alocado
-		getch(); 
-		return 0;
+int main(void)
+{
+	  clock_t t;
+	  int menu2,menu1, terceiroMenu,menuImpressao, retornoMenu1, retornoMenu2, retTeste,retornoMenu3;
+	  int *Vetor; //definindo o ponteiro v
+	  int i, tamanhoVetor;
+	  retornoMenu1 = MENU1(tamanhoVetor);
+	  Vetor = (int *) malloc(retornoMenu1 * sizeof(int)); //Armazenando os dados em um vetor 
+	  retornoMenu2 = MENU2(retTeste);
+	  popularVetor(retornoMenu2, retornoMenu1, Vetor, i);
+	  retornoMenu3 = MENU3(menu2);
+	  ordenamentos(Vetor, retornoMenu1, retornoMenu3);
+	 
+	  printf ("\nDigite 1 p/ Imprimir Valores\n");
+	  scanf("%d", &terceiroMenu);
+	  if(terceiroMenu==1 ){ 
+		  for (i = 0;i < retornoMenu1; i++) //Impressao dos Metodos
+			{
+				if(Vetor[i]<0){
+					return 0;
+				}
+				printf("%d\n",Vetor[i]);
+			}
+	  }
+	  else{
+	  	printf("erro!");
+	  	return 0;
+	  }
+	  printf("\n Rodolfo Casemiro Ribeiro - LUZ 2020 \n");
+	  free(Vetor); //liberando o espaço de memória alocado 
+	  return 0;
 }
